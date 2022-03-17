b57855e7977dfcfb90bc63c0e2076f24d2b620fb
horizD <he1236@ajou.ac.kr>
2022-03-17 17:56:22 +0900

    asgmt1 ~

--- Assignment_Individual/asgmt_1_201823869.py
+++ Assignment_Individual/asgmt_1_201823869.py
@@ -1,22 +1,28 @@
-# 1. convertCelsius
-def convertCelsius() :
-    Celsius = float(input('Enter a temperature degree in Celsius : '))
-    Fahrenheit =  Celsius
-    print('%f degrees Fahrenheit'%(Fahrenheit))
-convertCelsius()
+# # 1. convertCelsius - 완료
+# def convertCelsius() :
+#     Cel = float(input('Enter a temperature degree in Celsius(ºC) : '))
+#     Fah =  Cel * ( 9/5 ) + 32
+#     print('%0.2f degrees Fahrenheit(ºF)'%(Fah))
+# convertCelsius()
     
-# 2. getLength - 완료
-def getLength() : 
-    inputStr = input('Enter a word or phrase: ')
-    print('The length of the input is %d characters'%(len(inputStr)))
-getLength()
+# # 2. getLength - 완료
+# def getLength() : 
+#     inputStr = input('Enter a word or phrase: ')
+#     print('The length of the input is %d characters'%(len(inputStr)))
+# getLength()
 
 # 3. quadraticRoot
 def quadraticRoot() :
-    q_root = int(input())
-    print(q_root)
+    inVal: int = int(input('Enter two coefficients of x2(= a) and x (= b) and value of the constant (= c), respectively : '))
+    q_root1: float = 0
+    q_root2: float = 0
+    print('The solutions include %0.1f and %0.1f'%(q_root1,q_root2))    
+quadraticRoot()
+
+# # 4. calcDistance - 완료
+# import math
+
+# def calcDistance(x1:int, y1:int, x2:int , y2 :int) :
+#     return math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
     
-# 4. calcDistance 
-def calcDistance() :
-    dist = int(input())
-    print(dist)
+# print(calcDistance(-1,15,7,-3))
