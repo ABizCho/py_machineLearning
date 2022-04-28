7e67d731d99393bd23cbacb3132e3a31a8f84af3
horizD <he1236@ajou.ac.kr>
2022-04-28 14:34:25 +0900

    Assignment#3 partA.Linear 롤백을 위한 PartB.Logi 백업

--- ML_PRAC/prac5_matplotlib/ML5_3_set_SubPlot.py
+++ ML_PRAC/prac5_matplotlib/ML5_3_set_SubPlot.py
@@ -86,4 +86,140 @@ plt.bar(carB2,carS2)
 
 plt.show()
 
+ 
+# 서브플롯 디테일 설정 : 
 
+    #히스토그램 예제 : ref my assignment #2
+    
+'''
+
+import matplotlib.pyplot as plt
+
+
+plt.style.use('Solarize_Light2')
+
+fig, ax = plt.subplots(2, 1)
+
+n,bins,patches = ax[0].hist(df_sal['TotalPay'],edgecolor='black',linewidth=1.2 )
+patches[1].set_facecolor('gold')
+
+n,bins,patches = ax[1].hist(df_sal['TotalPay'],edgecolor='black',linewidth=1.2,bins=30 )
+patches[0].set_facecolor('gold')
+patches[3].set_facecolor('gold')
+
+ax[0].set_title("Histogram of TotalPay (default)")
+ax[1].set_title("Histogram of TotalPay (bins=30)")
+
+plt.xlabel('TotalPay')
+plt.ylabel('Count of records')
+
+fig.set_size_inches(9, 9)
+plt.show()
+
+
+    # 서브플롯 : https://teddylee777.github.io/visualization/matplotlib-tutorial
+    # 서브플롯 타이틀 : https://www.delftstack.com/ko/howto/matplotlib/how-to-add-title-to-subplots-in-matplotlib/
+    
+    # 히스토그램 특정 구간 강조 : https://velog.io/@khnn/TIL-Matplotlib%EC%9C%BC%EB%A1%9C-%ED%9E%88%EC%8A%A4%ED%86%A0%EA%B7%B8%EB%9E%A8-%EA%BE%B8%EB%AF%B8%EA%B8%B0
+
+'''
+
+
+    #라인차트 예제 : ref my assignment #2
+'''
+TP_year = df_sal.groupby('Year').mean().index.values
+TP_avg = df_sal.groupby('Year').mean()['TotalPay'].values
+
+xpoints = TP_year
+ypoints = TP_avg
+
+
+fig = plt.figure(figsize=(7,7)) # canvas
+ax = fig.add_subplot()          # frame
+
+ax.spines['right'].set_visible(False) # unvisible right spine
+ax.spines['top'].set_visible(False) # # unvisible top spine
+
+ax.plot(xpoints,ypoints, 'g', marker='o',linestyle='--')
+
+ax.axhline( TP_avg.mean(), label='mean')
+ax.text(2011,TP_avg.mean()+20, 'Mean : %f'%(TP_avg.mean()),fontsize=9,color='gray')
+
+plt.title('Avgs of TotalPay by years')
+
+plt.xticks(TP_year)
+plt.yticks(TP_avg)
+
+plt.xlabel('Years')
+plt.ylabel('Averages of whole TotalPay')
+plt.show()
+
+
+    # linear graph styling(goodRef): https://zephyrus1111.tistory.com/21
+'''
+
+
+    # bar chart 예제-1 : ref my assignment#3
+'''
+fig, ax = plt.subplots(2, 1)
+
+# WAY 1.
+    # pd.cut() method by default options
+df_sal['pay_class'] = pd.cut(df_sal['TotalPay'],3, labels=['low','medium','high'])
+
+df_sal_class = df_sal.groupby('pay_class')['pay_class'].count()
+
+ax[0].bar(df_sal_class.index.values,df_sal_class.values)
+ax[0].set_title('Way 1 : Dividing classes by default cut() options')
+
+
+# WAY 2.
+    # Dividing 3 class in the same range of value ( Using cut() method too) 
+
+endP = int(df_sal['TotalPay'].max())
+startP = int(df_sal['TotalPay'].min())
+
+
+intv = int((endP - startP) / 3)
+
+bins = range(startP,endP,intv)
+bins_label = ['low','medium','high']
+
+df_sal['pay_class'] = pd.cut( df_sal['TotalPay'], bins,right=False, labels=bins_label )
+
+ax[1].bar(df_sal_class.index.values,df_sal_class.values)
+ax[1].set_title('Way 2 : Dividing classes by the same range of value')
+
+
+
+plt.xlabel('TotalPay Class')
+plt.ylabel('Count of obs')
+
+print('Showing Counts of classes of Total pay bar chart...')
+
+fig.set_size_inches(9, 9)
+plt.show()
+'''
+
+
+    # bar chart 예제 - 2 : ref my assignment #2
+'''
+# Additional : Top10 the most paid jobtitles
+div(17,'Additional Analysis 2 : Top10 the most paid jobtitles ')
+import numpy as np
+top10_paidJts = df_sal.groupby('JobTitle').mean()['TotalPayBenefits'].sort_values(ascending=False)[0:10].astype(int)
+print(top10_paidJts)
+
+sp()
+
+fig, ax = plt.subplots(figsize=(8,8))
+
+ax.bar(top10_paidJts.index.str.lower(),top10_paidJts.values,
+       color = 'red',alpha=0.5)
+plt.title('Additional Analysis 2 : Top10 the most paid jobtitles')
+
+plt.subplots_adjust(bottom=0.5)
+plt.xticks(rotation=75)
+print('Showing Top10 the most paid jobtitles...')
+plt.show()
+'''
