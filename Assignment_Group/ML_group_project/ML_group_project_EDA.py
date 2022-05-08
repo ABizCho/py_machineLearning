c7da0e262034f43ed1c1b3a9009be81e2c0c536f
horizD <he1236@ajou.ac.kr>
2022-05-09 02:02:14 +0900

    ML_group_project, MY EDA 수행 및 인사이트 발굴

---
+++ Assignment_Group/ML_group_project/ML_group_project_EDA.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import numpy as np
+
+df_ht = pd.read_csv('C:/Users/he125/OneDrive/바탕 화면/Dev/Git/py_machineLearning/Assignment_Group/ML_group_project/hotel_bookings.csv')
+
+print(df_ht.head())
+
