7ba034b1fd7f7150aa90511fed7bc25b12bd3f68
horizD <he1236@ajou.ac.kr>
2022-05-11 13:40:05 +0900

    Updated ML GR PJ EDA

--- Assignment_Group/ML_group_project/ML_group_project_EDA.py
+++ Assignment_Group/ML_group_project/ML_group_project_EDA.py
@@ -1,7 +1,21 @@
-import pandas as pd
-import numpy as np
-
-df_ht = pd.read_csv('C:/Users/he125/OneDrive/바탕 화면/Dev/Git/py_machineLearning/Assignment_Group/ML_group_project/hotel_bookings.csv')
-
-print(df_ht.head())
-
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "read"
+   ]
+  }
+ ],
+ "metadata": {
+  "language_info": {
+   "name": "python"
+  },
+  "orig_nbformat": 4
+ },
+ "nbformat": 4,
+ "nbformat_minor": 2
+}
